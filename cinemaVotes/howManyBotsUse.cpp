dde4b092d7e692bf567ce2ea757d26e4a717d95a
fenya <dim74.09@mail.ru>
2022-10-11 01:28:23 +0500

    cinema votes

--- cinemaVotes/howManyBotsUse.cpp
+++ cinemaVotes/howManyBotsUse.cpp
@@ -19,19 +19,29 @@
 #include <iostream>
 using namespace std;
 int main() {
+	const double e = 0.000000001;
+	
 	float X = 0;
 	float Y = 0;
 	int N = 0;
 	int K = 0;
 	
-	cout << "X:";
-	cin >> X;
-	X = X + 0.5 - 0.00000001;
-	cout << "Y:";
-	cin >> Y;
-	cout << "N:";
-	cin >> N;
+	cout << "X:"; cin >> X;
+	cout << "Y:"; cin >> Y;
+	cout << "N:"; cin >> N;
+	
+	if(X == Y) {
+		cout << 0 << endl;
+		return 0;
+	} else if (Y == 0) {
+		cout << "error." << endl;
+		return 0;		
+	}	
+	
+	X = X + 0.5 - e;
+	Y = Y + 0.5 - e;
 
 	K = ((N*(X-Y))/Y)/1 + 1;
 	cout << K << endl;
+	return 0;
 }
